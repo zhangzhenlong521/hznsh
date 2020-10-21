75213fcf65eeea6a32b56db48debfa7e129b5a4a
zhangzhenlong521 <17744571124@163.com>
2020-10-21 19:18:11 +0800

    zzl

--- ipushgrc-new/src/com/pushworld/icase/ui/p010/AbstractBillListQueryCallback.java
+++
@@ -1,7 +0,0 @@
-package com.pushworld.icase.ui.p010;
-
-public abstract class AbstractBillListQueryCallback {
-	
-	public abstract void queryCallback();
-
-}
--- ipushgrc-new/src/com/pushworld/ipushgrc/ui/target/p030/TargetSystemImportIFC.java
+++
@@ -1,9 +0,0 @@
-package com.pushworld.ipushgrc.ui.target.p030;
-/**
- * 指标系统抽取 接口！
- * @author hm
- *
- */
-public interface TargetSystemImportIFC {
-	public void compute() throws Exception;
-}
