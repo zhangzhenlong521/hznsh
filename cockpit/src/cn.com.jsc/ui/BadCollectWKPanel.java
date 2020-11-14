3423fbedf1984f654f4c8219651e86e0801307ac
zhangzhenlong521 <17744571124@163.com>
2020-11-14 17:43:44 +0800

    zzl

---
+++ cockpit/src/cn.com.jsc/ui/BadCollectWKPanel.java
@@ -0,0 +1,14 @@
+package cn.com.jsc.ui;
+
+import cn.com.infostrategy.ui.mdata.BillListPanel;
+
+/**
+ * zzl
+ * 客户经理清收不良贷款排名
+ */
+public class BadCollectWKPanel {
+    BillListPanel billListPanel=new BillListPanel("BSD_GAGEISSUE_CODE1");
+    public BillListPanel getBillListPanel(){
+        return billListPanel;
+    }
+}
