3ff2aa6cc445fa63bd942ea7f2671354025083b1
zhangzhenlong521 <17744571124@163.com>
2020-11-14 15:29:32 +0800

    zzl提交BS工具类

---
+++ cockpit/src/cn.com.jsc/ui/CockpitServiceIfc.java
@@ -0,0 +1,10 @@
+package cn.com.jsc.ui;
+
+import cn.com.infostrategy.ui.common.WLTRemoteCallServiceIfc;
+
+/**
+ * zzl
+ * 数据可视化接口
+ */
+public interface CockpitServiceIfc extends WLTRemoteCallServiceIfc {
+}
---
+++ cockpit/src/cn/com/jsc/bs/CockpitServiceImpl.java
@@ -0,0 +1,6 @@
+package cn.com.jsc.bs;
+
+import cn.com.jsc.ui.CockpitServiceIfc;
+
+public class CockpitServiceImpl implements CockpitServiceIfc {
+}
