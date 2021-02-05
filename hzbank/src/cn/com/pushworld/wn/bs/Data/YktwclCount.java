b6715f0a007e9cb31f5f4f9b737aba273c68cf44
Feng-Jiajia <578770934@qq.com>
2021-02-05 17:30:04 +0800

    Feng Jia

--- hzbank/src/cn/com/pushworld/wn/bs/Data/YktwclCount.java
+++ hzbank/src/cn/com/pushworld/wn/bs/Data/YktwclCount.java
@@ -14,16 +14,17 @@ public class YktwclCount implements WLTJobIFC{
 	@Override
 	public String run() throws Exception {
         String [] column=dmo.getStringArrayFirstColByDS(null,"select COLUMN_NAME from user_tab_columns  WHERE TABLE_NAME='hzdb.hz_ykt_rate_"+getQYTTime()+"' ORDER BY COLUMN_ID");
-//        HashVO[] vos = dmo.getHashVoArrayByDS(null, )
+//			HashVO[] vos = dmo.getHashVoArrayByDS(null,"select * from ");
         if(column.length>0){
-        	return "一卡通数据表hz_ykt_rate_"+getQYTTime()+"表已存在，请删除后重新执行";
+        	return getQYTTime()+"数据已导入到系统";
+
         }else{
-        	dmo.executeUpdateByDS(null,"create table hzbank.hz_ykt_rate_"+getQYTTime()+" as select a.a,b.* from (select * from hzdb.excel_tab_164) a,(select * from hzbank.t_hnyzt_dis_"+getQYlTTime() +")b where (a.b||a.c)=(b.a003||b.a004)");
+        	dmo.executeUpdateByDS(null,"create table hzbank.hz_ykt_rate_"+getQYTTime()+" as select a.a name,b.* from (select * from hzdb.excel_tab_164) a,(select * from hzbank.t_hnyzt_dis_"+getQYlTTime() +" where load_dates='"+getQYTTime()+"')b where (a.b||a.c)=(b.a003||b.a004)");
         		dmo.executeUpdateByDS(null,"create or replace view v_hz_ykt_rate_"+getQYTTime()+" as (select a.name,a.c as yktzyk,b.c as yktptk,c.c as ykahfs,d.c as yktcount from " +
-        				"(select A name,count(name) as c from hzbank.hz_ykt_rate_"+getQYTTime()+" where A014='是' and A015='是' and A020='是' and A021='是' group by name) a left join " +
-        				"(select A name,count(name) as c from hzbank.hz_ykt_rate_"+getQYTTime()+" where A014='是' and A015='是' and A020='是' and A021='否' group by name) b on a.name=b.name left join " +
-        				"(select A name,count(name) as c from hzbank.hz_ykt_rate_"+getQYTTime()+" where A018='是' group by name) c on b.name=c.name left join " +
-        				"(select A name,count(name) as c from hzbank.hz_ykt_rate_"+getQYTTime()+" group by name) d on c.name=d.name)");
+        				"(select name,count(name) as c from hzbank.hz_ykt_rate_"+getQYTTime()+" where A014='是' and A015='是' and A020='是' and A021='是' group by name) a left join " +
+        				"(select name,count(name) as c from hzbank.hz_ykt_rate_"+getQYTTime()+" where A014='是' and A015='是' and A020='是' and A021='否' group by name) b on a.name=b.name left join " +
+        				"(select name,count(name) as c from hzbank.hz_ykt_rate_"+getQYTTime()+" where A018='是' group by name) c on b.name=c.name left join " +
+        				"(select name,count(name) as c from hzbank.hz_ykt_rate_"+getQYTTime()+" group by name) d on c.name=d.name)");
     		return "一卡通数据处理成功";
         }
 		
