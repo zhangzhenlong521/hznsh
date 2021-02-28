a155af5929dfb5388291fe6a5f4942eeb9eaf8d2
Feng-Jiajia <578770934@qq.com>
2021-02-28 15:08:10 +0800

    Feng Jia qiweiyiti

---
+++ hzbank/src/cn/com/pushworld/wn/ui/hz/table/TableGtGshStateWKPanel.java
@@ -0,0 +1,116 @@
+package cn.com.pushworld.wn.ui.hz.table;
+
+import cn.com.infostrategy.ui.common.AbstractWorkPanel;
+import cn.com.infostrategy.ui.common.UIUtil;
+import cn.com.infostrategy.ui.report.BillCellPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * TableGtGshStateWKPanel
+ * fj
+ * 个体工商户金融服务情况表
+ * @author Dragon
+ * @date 2021/2/26
+ */
+public class TableGtGshStateWKPanel extends AbstractWorkPanel implements ActionListener {
+    private BillCellPanel billCellPanel=null;
+    private String tablename;
+    private String dates=null;
+    @Override
+    public void initialize() {
+        billCellPanel=new BillCellPanel("s_table_gtgsh" ,null, null,true, false,true);
+        try{
+            getTablename();
+            billCellPanel.setValueAt("dates","数据日期:"+dates);
+        }catch (Exception e){
+
+        }
+        this.add(billCellPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+    /**
+     * 得到指定的月份的日期
+     *
+     * @return
+     */
+    public void getdates(String date) {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        try {
+            cal.setTime(format.parse(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat format2 = new SimpleDateFormat("yyyy年MM月dd日");
+        dates = format2.format(cal.getTime());
+    }
+    public Boolean getTablename(){
+        SimpleDateFormat formatTemp = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date1= null;
+        String createDate=null;
+        try {
+//            date1 = formatTemp.parse(""+getKHDQMonth()+" 10:30:00");
+            createDate= UIUtil.getStringValueByDS(null,"select CREATED from dba_objects where object_name = 'GRID_DATA_"+getQYTTime()+"' and OBJECT_TYPE='TABLE'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(createDate==null || createDate.equals("") || createDate.equals(null)){
+            tablename="grid_data_"+getQYTTime2();
+            getdates(getQYTTime2());
+            return false;
+        }else{
+            tablename="GRID_DATA_"+getQYTTime();
+            getdates(getQYTTime());
+            return true;
+        }
+//        Date date2=new Date();
+//        if(date1.getTime()>date2.getTime()){
+//            tablename="grid_data_"+getQYTTime2();
+//            return false;
+//        }else{
+//            tablename="GRID_DATA_"+getQYTTime();
+//            return true;
+//        }
+
+    }
+    /**
+     * 得到前一天的日期
+     *
+     * @return
+     */
+    public String getQYTTime() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        Date d = new Date();
+        cal.setTime(d);
+        int day = cal.get(Calendar.DATE);
+        cal.set(Calendar.DATE, day - 1);
+        String lastDate = format.format(cal.getTime());
+        return lastDate;
+    }
+    /**
+     * 得到前2天的日期
+     *
+     * @return
+     */
+    public String getQYTTime2() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        Date d = new Date();
+        cal.setTime(d);
+        int day = cal.get(Calendar.DATE);
+        cal.set(Calendar.DATE, day - 2);
+        String lastDate = format.format(cal.getTime());
+        return lastDate;
+    }
+}
